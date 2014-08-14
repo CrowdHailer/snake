812412943af9e3a011141f4cc681a997b88579ff
Peter <peterhsaxton@gmail.com>
2014-08-14 23:39:55 +0100

    simple cleave for array

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ require('./spec/stream_spec');
 require('./spec/each_spec');
 require('./spec/map_spec');
 require('./spec/filter_spec');
+require('./spec/cleave_spec');
 require('./spec/fromEvents_spec');
 require('./spec/mapStream_spec');
 require('./spec/filterStream_spec');
