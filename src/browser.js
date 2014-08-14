4e43fed21ab8465da109ccddc880440490529f89
Peter <peterhsaxton@gmail.com>
2014-08-14 22:48:00 +0100

    filter added to example

--- src/browser.js
+++ src/browser.js
@@ -3,6 +3,7 @@ window.Stream = require('./stream');
 window._ = {
     each: require('./each'),
     map: require('./map'),
+    filter: require('./filter'),
     log: console.log.bind(console),
     dot: require('cumin-dot')
 }
