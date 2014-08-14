5118fd7ad6b799b972bd37188ec380249782af2c
Peter <peterhsaxton@gmail.com>
2014-08-14 13:37:42 +0100

    test map arguments

--- test/spec/map_spec.js
+++ test/spec/map_spec.js
@@ -1,20 +1,24 @@
 'use strict';
 
 describe('Map', function () {
-    var map, action, result;
+    var map, dummy, result;
 
     beforeEach(function () {
         map = require('../../src/map');
-        action = function (x) {
-            return x * 2;
-        }
+        dummy = jasmine.createSpy().and.returnValue(1)
     });
 
     describe('for Arrays', function () {
         it('should map all elements to new array', function () {
             var array = [4, 2];
-            result = map(action, array)
-            expect(result).toEqual([8, 4])
+            result = map(dummy, array)
+            expect(result).toEqual([1, 1])
+        });
+
+        it('should call the action with element, index and array', function () {
+            var array = [4, 2];
+            map(dummy, array)
+            expect(dummy.calls.allArgs()).toEqual([[4, 0, array], [2, 1, array]])
         });
     });
 });
