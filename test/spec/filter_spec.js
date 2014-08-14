2b3f910b8853c2523809d86adc43065bdfb14cde
Peter <peterhsaxton@gmail.com>
2014-08-14 22:09:28 +0100

    check conditional calls

--- test/spec/filter_spec.js
+++ test/spec/filter_spec.js
@@ -6,6 +6,7 @@ describe('Filter', function () {
     beforeEach(function () {
         filter = require('../../src/filter');
         positive = function (n) { return n >= 0; }
+        dummy = jasmine.createSpy();
     });
 
     describe('for Arrays', function () {
@@ -13,5 +14,11 @@ describe('Filter', function () {
             var array = [-1, 1];
             expect(filter(positive, array)).toEqual([1])
         })
+
+        it('should call the action with element, index and array', function () {
+            var array = [-1, 1];
+            filter(dummy, array);
+            expect(dummy.calls.allArgs()).toEqual([[-1, 0, array], [1, 1, array]]);
+        });
     });
 });
